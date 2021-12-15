596d95ae45384d575348894c01f759a850012e11
Szymon Kolakowski <szymon.kolakowski@gmail.com>
2021-12-15 01:21:35 +0100

    #16 cpp types setup

---
+++ types.h
@@ -0,0 +1,17 @@
+#ifndef __TYPES_H
+#define __TYPES_H
+
+	// char is a 8 bit integer
+	typedef char int8_t;
+	typedef unsigned char uint8_t;
+
+	typedef short int16_t;
+	typedef unsigned short uint16_t;
+
+	typedef int int32_t;
+	typedef unsigned int uint32_t;
+
+	typedef long long int int64_t;
+	typedef unsigned long long int uint64_t;
+
+#endif
